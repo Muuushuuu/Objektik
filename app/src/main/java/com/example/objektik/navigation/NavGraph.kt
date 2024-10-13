58067192a92107fb4a255a82cc976923d749ff08
/Mushu <118520201+Muuushuuu@users.noreply.github.com>
2024-10-13 11:11:52 +0200

    Seconde page (Game) + navigation

---
+++ app/src/main/java/com/example/objektik/navigation/NavGraph.kt
@@ -0,0 +1,29 @@
+package com.example.objektik.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.objektik.screen.HomeScreen
+import com.example.objektik.screen.GameScreen
+
+@Composable
+fun NavGraph(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = "home"  // Screen de départ
+    ) {
+        // Composable HomeScreen
+        composable("home") {
+            HomeScreen(
+                onStartClick = { navController.navigate("game") }  // Démarer GameScreen
+            )
+        }
+
+        // Composable GameScreen
+        composable("game") {
+            GameScreen()
+        }
+    }
+}
--- app/src/main/java/com/example/objektik/screen/MainActivity.kt
+++ app/src/main/java/com/example/objektik/screen/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.objektik.navigation.NavGraph
 import com.example.objektik.ui.theme.ObjektikTheme
 
 class MainActivity : ComponentActivity() {
@@ -14,18 +16,18 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ObjektikTheme {
-                // Appel de la fonction composable HomeScreen
-                HomeScreen(onStartClick = { })
+                val navController = rememberNavController()
+                NavGraph(navController = navController)
             }
         }
     }
 }
 
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ObjektikTheme {
-        HomeScreen {  }
-    }
-}
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    ObjektikTheme {
+//        HomeScreen {  }
+//    }
+//}
