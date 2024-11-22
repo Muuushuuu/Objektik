8dd20eb3c745afeb475f23be31fc896191460ffd
/Mushu <118520201+Muuushuuu@users.noreply.github.com>
2024-11-22 18:32:09 +0100

    Mise a jour / organisation NavGraph

--- app/src/main/java/com/example/objektik/navigation/NavGraph.kt
+++ app/src/main/java/com/example/objektik/navigation/NavGraph.kt
@@ -11,52 +11,59 @@ import com.example.objektik.screen.GameScreen
 import com.example.objektik.screen.SuccessScreen
 import com.example.objektik.services.PointsManager
 
+// Définition des routes
+object Routes {
+    const val HOME = "home"
+    const val GAME = "game"
+    const val SUCCESS = "success/{nomFrancais}"
+    const val ERROR = "error/{nomFrancais}"
+}
+
 /**
- * NavGraph - Un composant permettant de gérer la navigation entre les différentes pages de l'application.
+ * NavGraph - Gère la navigation entre les écrans de l'application.
  *
- * @param navController Le contrôleur de navigation utilisé pour gérer les transitions entre les écrans.
+ * @param navController Contrôleur pour gérer les transitions entre les écrans.
+ * @param pointsManager Gestionnaire des points accumulés dans l'application.
  */
 @Composable
 fun NavGraph(navController: NavHostController, pointsManager: PointsManager) {
     NavHost(
         navController = navController,
-        startDestination = "home"  // Écran de départ
+        startDestination = Routes.HOME  // Écran affiché au démarrage
     ) {
         // Composable HomeScreen
-        composable("home") {
+        composable(Routes.HOME) {
             HomeScreen(
-                onStartClick = { navController.navigate("game") },  // Démarrer GameScreen
+                onStartClick = { navController.navigate(Routes.GAME) },  // Démarrer GameScreen => l'écran de jeu
                 points = pointsManager.getPoints()
             )
         }
 
         // Composable GameScreen
-        composable("game") {
+        composable(Routes.GAME) {
             GameScreen(navController = navController)
         }
 
         // Composable SuccessScreen : affiché lorsqu'un objet a été trouvé avec succès
-        composable("success/{nomFrancais}") { backStackEntry ->
+        composable(Routes.SUCCESS) { backStackEntry ->
             val nomFrancais = backStackEntry.arguments?.getString("nomFrancais")
             nomFrancais?.let {
                 SuccessScreen(
                     nomFrancais = it,
-                    onStartClick = {
-                        navController.navigate("game")
-                    },
-                    onAddPoints = { points ->  // Ajout des points
-                        pointsManager.addPoints(points)
-                    },
+                    onStartClick = { navController.navigate(Routes.GAME) }, // Rejouer
+                    onAddPoints = { points -> pointsManager.addPoints(points) }, // Ajoute des points
                     points = pointsManager.getPoints()
                 )
             }
         }
 
         // Composable ErrorScreen : affiché lorsqu'aucun objet n'a été trouvé
-        composable("error/{nomFrancais}") { backStackEntry ->
+        composable(Routes.ERROR) { backStackEntry ->
             val nomFrancais = backStackEntry.arguments?.getString("nomFrancais")
             nomFrancais?.let {
-                ErrorScreen(nomFrancais = it, onStartClick = { navController.navigate("game") })
+                ErrorScreen(
+                    nomFrancais = it,
+                    onStartClick = { navController.navigate(Routes.GAME) })
             }
         }
     }
