273ea0d6d500df1e062282f19d9ac43237b5886e
/Mushu <118520201+Muuushuuu@users.noreply.github.com>
2024-11-22 18:39:03 +0100

    Mise a jour / organistion ErrorScreen

--- app/src/main/java/com/example/objektik/screen/ErrorScreen.kt
+++ app/src/main/java/com/example/objektik/screen/ErrorScreen.kt
@@ -2,20 +2,16 @@ package com.example.objektik.screen
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.absoluteOffset
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
@@ -26,30 +22,27 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
-import androidx.navigation.NavHostController
 import com.example.objektik.R
 import com.example.objektik.ui.components.CustomButton
 import com.example.objektik.ui.components.CustomPopup
-import com.example.objektik.ui.theme.BluePrimary
 import com.example.objektik.ui.theme.ErrorColor
-import com.example.objektik.ui.theme.GreenAccent
-import com.example.objektik.ui.theme.ObjektikTheme
-import com.example.objektik.ui.theme.PrimaryText
 
 /**
  * ErrorScreen - Un composant pour afficher la page d'erreur (en cas de différence entre objet(s) de la photo et objet chercher)
  *
- * @param nomFrancais Le nom en francais de l'objet à chercher
+ * @param nomFrancais Nom de l'objet recherché (en français), affiché pour guider l'utilisateur.
+ * @param onStartClick Action déclenchée lorsqu'on clique sur le bouton "Réessayer".
  */
 @Composable
 fun ErrorScreen(nomFrancais: String, onStartClick: () -> Unit) {
+    // Conteneur principal
     Box(
         modifier = Modifier.fillMaxSize()
     ){
+        // Carte contenant le message d'erreur
         Box(
             modifier = Modifier
                 .fillMaxSize()
@@ -63,12 +56,13 @@ fun ErrorScreen(nomFrancais: String, onStartClick: () -> Unit) {
                     .padding(16.dp)
                     .fillMaxSize()
             ) {
+                // Popup personnalise pour afficher un titre d'erreur
                 CustomPopup(
                     text = "Oups ! \uD83D\uDE4A",
                     borderColor = ErrorColor,
                     textColor = ErrorColor,
                 )
-
+                // Contenu principal de l'erreur
                 Box(
                     modifier = Modifier
                         .fillMaxSize()
@@ -81,16 +75,15 @@ fun ErrorScreen(nomFrancais: String, onStartClick: () -> Unit) {
                         verticalArrangement = Arrangement.Center,
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
+                        // Image d'emoji triste
                         Image(
                             painter = painterResource(id = R.drawable.sad),
                             contentDescription = "Image emoji sad",
                             modifier = Modifier.size(150.dp),
                             contentScale = ContentScale.Crop
                         )
-                        Spacer(
-                            modifier = Modifier.fillMaxWidth()
-                                .height(50.dp))
-
+                        Spacer( modifier = Modifier.fillMaxWidth().height(50.dp) )// Espace entre l'image et le texte
+                        // Message d'erreur
                         Text(
                             text = "L’objet n’a pas été reconnu, essayez encore ! \nL'objet à trouver est \"$nomFrancais\"",
                             fontSize = 18.sp,
@@ -101,8 +94,8 @@ fun ErrorScreen(nomFrancais: String, onStartClick: () -> Unit) {
 
                 }
             }
-
         }
+        // Bouton pour réessayer
         CustomButton(
             text = "Réessayer",
             onClick = onStartClick,
